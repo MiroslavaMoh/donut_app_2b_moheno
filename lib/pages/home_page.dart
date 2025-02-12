603352e49e01e9616e6fe962fe8e0ce8e0ad05eb
MiroslavaMoh <miroslavamoh252@gmail.com>
2025-02-12 11:32:07 -0600

    TabBar Added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:donut_app_2b_moheno/utils/my_tab.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,17 +9,52 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //ista de tabs
+  List<Widget>myTabs=[
+    MyTab(iconPath:'lib/icons/icons/burger.png'),
+    //E:\Users\DELL\Downloads\unidad_2\donut_app_2b_Moheno\donut_app_2b_moheno\lib\icons\icons\burger.png
+    MyTab(iconPath:'lib/icons/icons/burger.png'),
+    MyTab(iconPath:'lib/icons/icons/smoothie.png'),
+    MyTab(iconPath:'lib/icons/icons/pancakes.png'),
+    MyTab(iconPath:'lib/icons/icons/pizza.png')
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        leading: Icon(Icons.menu,
-        color: Colors.grey[800],
+    return DefaultTabController(
+      length:5,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          leading: Icon(Icons.menu,
+          color: Colors.grey[800],
+          ),
+          //Icono derecho
+          actions: [Padding(
+            padding: const EdgeInsets.only(right: 24.0),
+            child: Icon(Icons.person),
+          )],
         ),
-        //Icono derecho
-        actions: [Icon(Icons.person)],
-      )
+        body: Column(
+          children: [
+            //Texto principal
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 36, vertical: 18),
+              child: Row(
+                children:[
+                  Text("Tengo el deseo de ingerir el sustento ", style:TextStyle(fontSize: 30)),
+                  Text("vital", style:TextStyle(fontSize:32,fontWeight:FontWeight.bold,decoration:TextDecoration.underline))
+                ]
+              ),
+            ),
+            //Tab bar
+            TabBar(tabs:myTabs)
+            //Tabbar view
+      
+            //Carrito
+      
+          ],)
+      ),
     );
   }
 }
